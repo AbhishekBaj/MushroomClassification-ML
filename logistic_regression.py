0ed37c9d55c9906b1c9f288970daff43e766d476
emmeline <emmeline.pearson@case.edu>
2023-12-09 13:30:16 -0500

    add validation data metrics

--- logistic_regression.py
+++ logistic_regression.py
@@ -69,12 +69,36 @@ X_val = z_score_standardization(X_val, mean, std)
 
 # training 
 learning_rate = 0.1
-epochs = 10500
+epochs = 10000
 small_value = 2**(-32)
 weights, mean_log_loss_history = run_logistic_regression(X_train, Y_train, epochs, learning_rate, small_value)
-predicted_val = classify_samples(X_val, weights)
 
-# Metrics
+
+# Metrics for training data 
+predicted_train = classify_samples(X_train, weights)
+
+# Metrics based on training data
+false_positive_train = np.sum((Y_train == 0) & (predicted_train == 1))
+false_negative_train = np.sum((Y_train == 1) & (predicted_train == 0))
+true_positive_train = np.sum((Y_train == 1) & (predicted_train == 1))
+true_negative_train = np.sum((Y_train == 0) & (predicted_train == 0))
+
+precision_train = true_positive_train / (true_positive_train + false_positive_train)
+recall_train = true_positive_train / (true_positive_train + false_negative_train)
+f1_score_train = 2 * (precision_train * recall_train) / (precision_train + recall_train)
+accuracy_train = (true_positive_train + true_negative_train) / len(Y_train)
+
+print("Validation Metrics:")
+print("precision: ", precision_train)
+print("recall: ", recall_train)
+print("f1_score: ", f1_score_train)
+print("accuracy: ", accuracy_train)
+print(" ")
+
+weights, mean_log_loss_history_train = run_logistic_regression(X_val, Y_val, epochs, learning_rate, small_value)
+
+# Metrics for training data 
+predicted_val = classify_samples(X_val, weights)
 false_positive = np.sum((Y_val == 0) & (predicted_val == 1))
 false_negative = np.sum((Y_val == 1) & (predicted_val == 0))
 true_positive = np.sum((Y_val == 1) & (predicted_val == 1))
@@ -85,15 +109,17 @@ recall = true_positive / (true_positive + false_negative)
 f1_score = 2 * (precision * recall) / (precision + recall)
 accuracy = (true_positive + true_negative) / len(Y_val)
 
+print("Training Metrics: ")
 print("precision: ", precision)
 print("recall: ", recall)
 print("f1_score: ", f1_score)
 print("accuracy: ", accuracy)
 
 # Plot
-plt.plot(range(len(mean_log_loss_history)), mean_log_loss_history)
-plt.ylabel("Log Loss")
-plt.xlabel("Epoch")
+plt.plot(range(len(mean_log_loss_history_train)), mean_log_loss_history_train, label='Training Log Loss')
+plt.plot(range(len(mean_log_loss_history)), mean_log_loss_history, label='Validation Log Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Log Loss')
+plt.title('Training and Validation Log Loss')
+plt.legend()
 plt.show()
-
-
